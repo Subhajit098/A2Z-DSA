8e675660e0696c4fabe36c41ecb1a4236836c436
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-12-26 13:00:49 +0530

    combination sum III

---
+++ Recursion/Subsequences_pattern/combination_sum_III.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertCombinations(int ind,int k,int sum,int n,vector<int>&ds,vector<vector<int>>&ans){
+        if(k==0 && sum==n){
+            ans.push_back(ds);
+            return;
+        }
+
+        if(k==0 && sum<n) return;
+
+        if(k>0 && sum>=n){
+            return;
+        }
+
+        for(int i=ind;i<=9;i++){
+            sum+=i;
+            ds.push_back(i);
+            insertCombinations(i+1,k-1,sum,n,ds,ans);
+            sum-=i;
+            ds.pop_back();
+        }
+
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int>ds;
+        vector<vector<int>>ans;
+        insertCombinations(1,k,0,n,ds,ans);
+        return ans;
+    }
